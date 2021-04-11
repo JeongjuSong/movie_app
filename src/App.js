77e4e4e4e1ebb09d1d5699470370e17f9821c31b
JeongjuSong <pc1541@naver.com>
2021-04-11 23:03:39 +0900

    #1.1 Second push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {}
+
+  // 만약 영화 앱을 만든다면 willMount()를 진행할 때 영화 api를 호출한다.
+  componentWillMount(){
+    console.log('will mount')
+
+  }
+
+
+  // 컴포넌트가 마운트 되면 5초 기다리고 state를 업데이트 하겠다.
+  componentDidMount() {
+    this._getMovies()
+  }
+
+
+   _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts-proxy.nomadcoders1.now.sh/list_movies.json')
+    .then( response => response.json() )
+    .then( json => json.data.movies )
+    .catch( err => console.log(err) )
+  }
+
+  // 리액트는 자체 기능이 많기 때문에 직접 만든 function과 차이점을 두기 위해 언더스코어를 사용
+  _renderMovies = () => {
+    console.log(this.state.movies)
+    const movies = this.state.movies.map(movie => {
+      // 리액트는 array 엘리먼트일 경우 unique key를 줘야함
+      return <Movie
+        title={movie.title_english}
+        poster={movie.medium_cover_image}
+        key={movie.id}
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+      />
+    })
+    return movies
+  }
+
+  render() {
+    return (
+      <div className={this.state.movies ? "App" : "App--loading"}>
+        {this.state.movies ? this._renderMovies() : 'Loading'}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM은 리액트를 웹사이트에 출력(render)하는걸 도와주는 모델
+// 리액트를 웹에 올리고 싶다면 -> reactDOM을 쓰고 리액트를 모바일에 올려놓고 싶다면 -> reactNative를 사용
+// 리액트 - UI 라이브러리 / 리액트DOM - 리액트를 웹사이트에 출력해 줌
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
